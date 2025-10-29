222dd222d63c1dbbf67526ea2a80cc852a5b4037
Filla Ramadhan <fillar71@gmail.com>
2025-10-30 06:44:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import timelineRoutes from "./routes/timelineRoutes.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
+// route utama
 app.use("/api/timeline", timelineRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Magistory Backend berjalan 🚀");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port " + (process.env.PORT || 5000));
 });
-
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("DB connection error:", err));
