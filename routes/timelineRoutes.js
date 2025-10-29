b030704dfbe533186737181ffc8baceaa3ac2fff
Filla Ramadhan <fillar71@gmail.com>
2025-10-30 04:23:21 +0700

    update routes/timelineRoutes.js

--- routes/timelineRoutes.js
+++ routes/timelineRoutes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { getTimeline, postTimeline } from "../controllers/timelineController.js";
 
+const router = express.Router();
+
+router.get("/", getTimeline);
+router.post("/", postTimeline);
+
+export default router;  // ✅ tambahkan baris ini
